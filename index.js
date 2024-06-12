ec83d0febd2157fc0e0902b4bc142568372ded15
juanmiitaklk <juanjuanjuan112014@gmail.com>
2024-06-13 00:48:59 +0200

    "Segundo commit"

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const body = document.querySelector("body"),
-        sidebar = body.querySelector("nav"),
-        toggle = body.querySelector(".toggle"),
-        searchBtn = body.querySelector(".search-box"),
-        modeSwitch = body.querySelector(".toggle-switch"),
-        modeText = body.querySelector(".mode-text");
+  const body = document.querySelector("body"),
+    sidebar = body.querySelector("nav"),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
 
-    toggle.addEventListener("click", () => {
-        sidebar.classList.toggle("close");
-    });
+  toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    document.querySelector(".content").classList.toggle("sidebar-close");
+  });
 
-    searchBtn.addEventListener("click", () => {
-        sidebar.classList.remove("close");
-    });
+  searchBtn.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+    document.querySelector(".content").classList.remove("sidebar-close");
+  });
 
-    modeSwitch.addEventListener("click", () => {
-        body.classList.toggle("dark");
-        if (body.classList.contains("dark")) {
-            modeText.innerText = "Modo Claro";
-        } else {
-            modeText.innerText = "Modo Oscuro";
-        }
-    });
-
+  modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    if (body.classList.contains("dark")) {
+      modeText.innerText = "Modo Claro";
+    } else {
+      modeText.innerText = "Modo Oscuro";
+    }
+  });
 });
